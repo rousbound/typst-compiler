3d104286cde7fe2fb5bcf6eda21b523628e1360f
Geraldo Luiz <geraldoluiz@puc-rio.br>
2023-05-15 16:04:52 -0300

    Initial commit

---
+++ tests/test.rs
@@ -0,0 +1,42 @@
+#[cfg(test)]
+mod tests {
+
+    use genpdf;
+    use serde_json::{Value, json};
+    use std::fs;
+
+    #[test]
+    fn test_api() {
+        if let Ok(data) = genpdf::genpdf("tests/test.typ".into(), ".".into(), None) {
+            fs::write("tests/test.pdf", data);
+        } else {
+            panic!("Test failed");
+
+        }
+    }
+
+    #[test]
+    fn test_api_with_json() {
+        let my_value: Value = json!({
+            "name": "John",
+            "age": 30,
+            "is_student": true,
+            "hobbies": ["reading", "coding", "hiking"],
+            "address": {
+                "street": "123 Main St",
+                "city": "Anytown",
+                "state": "CA",
+                "zip": "12345"
+            }
+        });
+
+        
+        if let Ok(data) = genpdf::genpdf("tests/test_with_json.typ".into(), ".".into(), Some(my_value)) {
+            fs::write("tests/test_with_json.pdf", data);
+        } else {
+            panic!("Test failed");
+
+        }
+    }
+
+}
